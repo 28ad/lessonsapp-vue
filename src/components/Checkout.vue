38974941bf186398201c6f3a99a1b5ce19f11ab3
28ad <alessiodita10@gmail.com>
2024-03-10 17:02:16 +0000

    update 2

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 <template>
     <h1>Checkout page</h1>
-=======
-<template>
-    <h1>Checkout page</h1>
->>>>>>> 0107cb4e971af421054acae723e112e2cc249c23
 </template>
--- src/components/LessonsList.vue
+++ src/components/LessonsList.vue
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 <template>
     <h1>Lessons page</h1>
-=======
-<template>
-    <h1>Lessons page</h1>
->>>>>>> 0107cb4e971af421054acae723e112e2cc249c23
 </template>
