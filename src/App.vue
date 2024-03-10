e9fbc0c742bb250624ac86764f92e8db67cba5bb
28ad <alessiodita10@gmail.com>
2024-03-10 20:21:38 +0000

    update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
-
 <template>
   <div id="app">
-    <button @click="showCheckout">{{ this.cart.length }}
-      Checkout</button>
-    <component :is="currentView"></component>
+    <!-- header of the page  -->
+    <div class="header">
+      <h1 v-if="showCheckout">BOOK YOUR LESSONS !</h1>
+      <h1 v-else>CART</h1>
+      <button id="checkoutBtn" class="checkout-btn" v-on:click="showCheckout">{{ cartCounter }}&nbsp;Checkout</button>
+    </div>
+
+    <component :is="currentView" :subjects="subjects" @add-to-cart="addToCart" :cart="cart"></component>
   </div>
 </template>
 
@@ -18,10 +22,25 @@ export default {
     return {
       cart: [],
       currentView: LessonsList,
-      
+      subjects: [],
+      serverURL: "https://webstore-restapi.onrender.com/collections/products/",
     }
   },
-  components: {LessonsList, Checkout},
+  components: { LessonsList, Checkout },
+  created: function () {
+    // Fetch data as soon as the homepage loads
+    fetch('https://webstore-restapi.onrender.com/collections/products')
+      .then(response => response.json())
+      .then(data => {
+        console.log('Data from MongoDB:', data);
+        // Use the data in your front-end application
+        this.subjects = data;
+
+        console.log('Data from MongoDB:', this.subjects);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+
+  },
   methods: {
     showCheckout() {
       if (this.currentView === LessonsList) {
@@ -29,9 +48,55 @@ export default {
       } else {
         this.currentView = LessonsList;
       }
-    }
-    
-  }
+    },
+    // function which adds items to the cart array
+    addToCart: function (subject, index) {
+
+      // search if item is already present in the cart array
+      let lessonIndex = this.cart.findIndex((ct) => ct.product_id === subject.product_id);
+
+      // if not
+      if (lessonIndex == -1) {
+
+        // add subject properties to cart
+        this.cart.push({
+          ...subject,
+          quantity: 1,
+        });
+
+      } else {
+
+        // add to cart quantity 
+        this.cart[lessonIndex].quantity++;
+      }
+
+      // subtract from the available inventory 
+      this.subjects[index].invQuantity--;
+
+      // enable cart button after item has been added
+      let checkoutBtn = document.getElementById("checkoutBtn");
+      if (checkoutBtn) {
+        checkoutBtn.removeAttribute("disabled");
+      }
+
+    },
+
+
+  },
+
+  computed: {
+        // updates cart count real time - shows empty string if cart is empty
+        cartCounter: function () {
+
+            let cartCount = 0;
+
+            for (const item of this.cart) {
+                cartCount += item.quantity;
+            }
+
+            return cartCount || "";
+        },
+}
 }
 </script>
 
--- src/components/LessonsList.vue
+++ src/components/LessonsList.vue
@@ -1,3 +1,201 @@
 <template>
-    <h1>Lessons page</h1>
+    <div>
+        <!-- Sort boxes container -->
+        <div class="sort-container">
+            <!-- Title -->
+            <select v-model="titleSort" @change="sortByTitle">
+                <option value="Default">Title</option>
+                <option value="Ascending">Ascending</option>
+                <option value="Descending">Descending</option>
+            </select>
+
+            <!-- Location -->
+            <select v-model="locationSort" @change="sortByLocation">
+                <option value="Location">Location</option>
+                <option value="Ascending">Ascending</option>
+                <option value="Descending">Descending</option>
+            </select>
+
+            <!-- Availability -->
+            <select v-model="availabilitySort" @change="sortByAvailability">
+                <option value="Spaces">Spaces</option>
+                <option value="Ascending">Low - High</option>
+                <option value="Descending">High - Low</option>
+            </select>
+
+            <!-- Price -->
+            <select v-model="priceSort" @change="sortByPrice">
+                <option value="Price">Price</option>
+                <option value="Ascending">Low - High</option>
+                <option value="Descending">High - Low</option>
+            </select>
+        </div>
+
+        <div class="wrap">
+            <div class="search">
+                <input v-model="searchTerm" @input="searchLessons" type="text" class="searchTerm"
+                    placeholder="What are you looking for?">
+                <button type="submit" class="searchButton">
+                    <i class="fa fa-search"></i>
+                </button>
+            </div>
+        </div>
+
+        <!-- Display search results -->
+        <div class="cards" v-if="searchResults.length > 0">
+            <div v-for="result in searchResults" :key="result._id">
+                <div class="lesson-container">
+                    <img :src="result.img" alt="Icon">
+                    <h2>{{ result.name }}</h2>
+                    <p>{{ result.location }}</p>
+                    <p>£ {{ result.price }}</p>
+                    <p>Spaces : {{ result.invQuantity }}</p>
+                    <button class="addToBasket" @click="addToCart(result, subjectIndex)"
+                        v-if="checkAvailability(result)">ADD LESSON</button>
+                    <button class="btn-disabled" disabled v-else>ADD LESSON</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Lesson page container -->
+        <div class="cards" v-else>
+            <div v-for="(subject, subjectIndex) in subjects" :key="subject.product_id">
+                <div class="lesson-container">
+                    <img :src="subject.img" alt="Icon">
+                    <h2>{{ subject.name }}</h2>
+                    <p>{{ subject.location }}</p>
+                    <p>£ {{ subject.price }}</p>
+                    <p>Spaces : {{ subject.invQuantity }}</p>
+                    <button class="addToBasket" @click="addToCart(subject, subjectIndex)"
+                        v-if="checkAvailability(subject)">ADD LESSON</button>
+                    <button class="btn-disabled" disabled v-else>ADD LESSON</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    name: "LessonsList",
+    props: {
+        subjects: Array,
+        cart: Array,
+    },
+    data() {
+
+        return {
+            titleSort: 'Default',
+            locationSort: 'Location',
+            availabilitySort: 'Spaces',
+            priceSort: 'Price',
+            searchTerm: '',
+            searchResults: [],
+        }
+    },
+    methods: {
+        // sort subjects by title
+
+        sortByTitle: function () {
+
+
+            // sort in ascending order alphabetically
+            if (this.titleSort === "Ascending") {
+
+                this.subjects.sort((first, second) => first.name.localeCompare(second.name));
+
+            } else if (this.titleSort === "Descending") { // sort in descending order
+
+                this.subjects.sort((first, second) => second.name.localeCompare(first.name));
+
+            }
+        },
+
+        // sort subjects by location
+
+        sortByLocation: function () {
+
+
+            // sort in ascending order alphabetically
+            if (this.locationSort === "Ascending") {
+
+                this.subjects.sort((first, second) => first.location.localeCompare(second.location));
+
+            } else if (this.locationSort === "Descending") { // sort in descending order
+
+                this.subjects.sort((first, second) => second.location.localeCompare(first.location));
+
+            }
+        },
+
+        // sort subjects by availability
+
+        sortByAvailability: function () {
+
+
+            // sort in ascending order
+            if (this.availabilitySort === "Ascending") {
+
+                this.subjects.sort((first, second) => first.invQuantity - second.invQuantity);
+
+            } else if (this.availabilitySort === "Descending") { // sort in descending order
+
+                this.subjects.sort((first, second) => second.invQuantity - first.invQuantity);
+
+            }
+        },
+
+        // sort subjects by price
+
+        sortByPrice: function () {
+
+
+            // sort in ascending order
+            if (this.priceSort === "Ascending") {
+
+                this.subjects.sort((first, second) => first.price - second.price);
+
+            } else if (this.priceSort === "Descending") { // sort in descending order
+
+                this.subjects.sort((first, second) => second.price - first.price);
+
+            }
+        },
+
+        searchLessons: function () {
+            // Use the searchTerm from the data property
+            const searchTerm = this.searchTerm.trim();
+
+            if (searchTerm === '') {
+                // If the search term is empty return list
+                location.reload();
+                console.log('Search term is empty.' + this.searchResults);
+                return;
+            }
+
+            // Perform the search using the updated searchTerm
+            fetch(`https://webstore-restapi.onrender.com/search/${searchTerm}`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Search results:', data);
+                    // Update the searchResults property to reflect the search results
+                    this.searchResults = data;
+                })
+                .catch(error => console.error('Error searching lessons:', error));
+        },
+
+        addToCart(subject, subjectIndex) {
+            this.$emit("add-to-cart", subject, subjectIndex);
+        },
+    },
+    computed: {
+       
+        checkAvailability: function () {
+            return function (subject) {
+                return subject.invQuantity > 0;
+            }
+        }
+
+    }
+};
+</script>
