edef41c52e19c866e43af49ce52cd3860b66e6a9
28ad <alessiodita10@gmail.com>
2024-03-10 16:59:33 +0000

    update

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+
+<template>
+  <div id="app">
+    <button @click="showCheckout">{{ this.cart.length }}
+      Checkout</button>
+    <component :is="currentView"></component>
+  </div>
+</template>
+
+<script>
+import LessonsList from './components/LessonsList.vue';
+import Checkout from './components/Checkout.vue';
+
+export default {
+  name: "app",
+  data() {
+
+    return {
+      cart: [],
+      currentView: LessonsList,
+      
+    }
+  },
+  components: {LessonsList, Checkout},
+  methods: {
+    showCheckout() {
+      if (this.currentView === LessonsList) {
+        this.currentView = Checkout;
+      } else {
+        this.currentView = LessonsList;
+      }
+    }
+    
+  }
+}
+</script>
+
---
+++ src/components/LessonsList.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Lessons page</h1>
+</template>
